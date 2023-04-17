bb5738531415cee5fccd931d9f84d8631575eadf
nguyen thanh long <loglog050801>
2023-04-17 20:56:10 +0700

    setup model , midd , route

--- routes/web.php
+++ routes/web.php
@@ -18,14 +18,27 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware(['auth','2fa','verified'])->group(function (){
+    Route::post('/2fa', function () {
+        return redirect(route('dashboard'));
+    })->name('2fa');
+
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/confirmPassword', [ProfileController::class, 'confirmPassword'])->name('profile.confirm_password');
+    Route::post('/confirmPassword', [ProfileController::class, 'confirmPasswordStore'])->name('profile.confirm_password.store');
+    Route::get('/showForm2fa',[ProfileController::class,'showForm2fa'])->name('profile.showForm2fa');
+    Route::post('/showForm2fa',[ProfileController::class,'showForm2faStore'])->name('profile.showForm2fa.store');
+    Route::get('/notEnable2fa',[ProfileController::class,'notEnable2fa'])->name('profile.notEnable2fa');
+
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
